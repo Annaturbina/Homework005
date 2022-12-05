4e38464c67d7900833f9340e35196c6f61d1723e
Anna <turbina.anna97@yandex.ru>
2022-12-05 17:03:50 +0100

    new folders

--- Program36/Program.cs
+++ Program36/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи число элементов в массиве:");
+int sum = 0;
+void SumNumbers()
+{
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-1000, 1000);
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+        if (i % 2 != 0)
+        {
+            sum = sum + array[i];
+        }
+    Console.WriteLine($"Сумма элементов массива, стоящих в нечётных позициях ={sum}");
+}
+SumNumbers();
